a9a04d42d7939fd2d825bd18a401be73aad3596e
Mohammad Ayaz <89903774+1Ayaz@users.noreply.github.com>
2023-12-14 10:07:43 +0530

    #include<stdio.h> int main() {     int num1,num2,quotient;     scanf("%d",&num1);     scanf("%d",&num2);     quotient = num1/num2 ;     printf("%d",quotient);     return 0; }

---
+++ Division.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+    int num1,num2,quotient;
+    scanf("%d",&num1);
+    scanf("%d",&num2);
+    quotient = num1/num2 ;
+    printf("%d",quotient);
+    return 0;
+}
