745b2256d675c63c5e3b2904e5fbe804896bfcf7
Mohammad Ayaz <89903774+1Ayaz@users.noreply.github.com>
2023-12-14 10:03:41 +0530

    #include<stdio.h> int main(){     int num1,num2,product;     scanf("%d",&num1);     scanf("%d",&num2);     product = num1 * num2 ;     printf("%d",product);     return 0;
    }

---
+++ Multiplication.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int num1,num2,product;
+    scanf("%d",&num1);
+    scanf("%d",&num2);
+    product = num1 * num2 ;
+    printf("%d",product);
+    return 0;
+    
+}
