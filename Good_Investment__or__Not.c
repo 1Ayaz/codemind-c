e83aecf59986adf9997ff45be6a61f1afb377b40
Mohammad Ayaz <89903774+1Ayaz@users.noreply.github.com>
2023-12-16 10:16:00 +0530

    #include<stdio.h> int main() {     int X,Y;     scanf("%d%d",&X ,&Y );     int good = Y*2 ;     if(X >= good)     {     printf("YES");     }     else     {         printf("NO");     }     return 0; }

---
+++ Good_Investment__or__Not.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() {
+    int X,Y;
+    scanf("%d%d",&X ,&Y );
+    int good = Y*2 ;
+    if(X >= good)
+    {
+    printf("YES");    
+    }
+    else
+    {
+        printf("NO");
+    }
+    return 0;
+}
+    
+    
