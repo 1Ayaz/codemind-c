22ff619150810d4f1bebd24f6f54188fb7e47ee0
Mohammad Ayaz <89903774+1Ayaz@users.noreply.github.com>
2023-12-16 11:56:54 +0530

    #include<stdio.h> int main () {     int X;
        scanf("%d",&X);
        if(X >= 18){
            printf("YES");
        }
        else
        {
            printf("NO");
        }
        return 0;
    }

---
+++ Casting_the_Vote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+    int X;
+
+    scanf("%d",&X);
+    if(X >= 18){
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    return 0;
+}
