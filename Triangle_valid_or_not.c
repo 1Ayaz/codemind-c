3064c52efbbc946826edfd59f1d8850fe200e2b5
Mohammad Ayaz <89903774+1Ayaz@users.noreply.github.com>
2023-12-18 14:50:39 +0530

    #include<stdio.h> int main () {     int S1,S2,S3;     scanf("%d%d%d",&S1 ,&S2 ,&S3);     if (S1 + S2 > S3 && S1 + S3 > S2 && S2 + S3 > S1)     {         printf("Valid triangle");     }     else     {         printf("Invalid triangle");     }     return 0; }

---
+++ Triangle_valid_or_not.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+    int S1,S2,S3;
+    scanf("%d%d%d",&S1 ,&S2 ,&S3);
+    if (S1 + S2 > S3 && S1 + S3 > S2 && S2 + S3 > S1)
+    {
+        printf("Valid triangle");
+    }
+    else
+    {
+        printf("Invalid triangle");
+    }
+    return 0;
+}
