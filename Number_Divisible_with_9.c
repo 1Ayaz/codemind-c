209a3eec8f68fa51f174e108ca7f004b2f275009
Mohammad Ayaz <89903774+1Ayaz@users.noreply.github.com>
2023-12-19 14:13:00 +0530

    #include<stdio.h> int main () {     int x;     scanf("%d",&x);     if (x%9 == 0)     {         printf("True");     }     else     {         printf("False");     }     return 0; }

---
+++ Number_Divisible_with_9.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+    int x;
+    scanf("%d",&x);
+    if (x%9 == 0)
+    {
+        printf("True");
+    }
+    else 
+    {
+        printf("False");
+    }
+    return 0;
+}
