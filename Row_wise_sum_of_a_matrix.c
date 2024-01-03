d8b0e2758102f5d3af000418a09a46f1b1e18863
Mohammad Ayaz <89903774+1Ayaz@users.noreply.github.com>
2024-01-03 11:45:31 +0530

    #include<stdio.h> int main() {     int a,b,i,j;     scanf("%d%d",&a,&b);     int ar[a][b];     for(i=0;i<a;i++)     { int sum=0;     for(j=0;j<b;j++)     {         scanf("%d",&ar[i][j]);         sum+=ar[i][j];         }         printf("%d ",sum); } }

---
+++ Row_wise_sum_of_a_matrix.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,j;
+    scanf("%d%d",&a,&b);
+    int ar[a][b]; 
+    for(i=0;i<a;i++)
+    { int sum=0;
+    for(j=0;j<b;j++)
+    { 
+        scanf("%d",&ar[i][j]);
+        sum+=ar[i][j];
+        } 
+        printf("%d ",sum);
+} }
