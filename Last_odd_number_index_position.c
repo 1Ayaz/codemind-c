a6bd1109b5e2905a6c2af03429dbf7331a7ca377
Mohammad Ayaz <89903774+1Ayaz@users.noreply.github.com>
2024-01-03 14:49:00 +0530

    #include<stdio.h> int main() { int a; scanf("%d",&a); int ar[a],s=0; for(int i=0;i<a;i++) { scanf("%d",&ar[i]); } for(int i=0;i<a;i++) { if(ar[i]%2==1) { s=i; } } printf("%d",s); }

---
+++ Last_odd_number_index_position.c
@@ -0,0 +1,11 @@
+#include<stdio.h> 
+int main()
+{ int a;
+scanf("%d",&a); 
+int ar[a],s=0;
+for(int i=0;i<a;i++)
+{ scanf("%d",&ar[i]);
+} for(int i=0;i<a;i++)
+{ if(ar[i]%2==1)
+{ s=i; } }
+printf("%d",s); }
